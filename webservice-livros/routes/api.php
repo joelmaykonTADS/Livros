f5d8b502d0c6d09c6d88cd70b6a89dba252e493b
Joel <joelmaykon94@gmail.com>
2019-07-07 14:50:23 -0300

    rota para acessar ao metodo cadastrar usuario usando controlador CadastroUsuarioController

--- webservice-livros/routes/api.php
+++ webservice-livros/routes/api.php
@@ -23,29 +23,7 @@ Route::middleware('auth:api')->get('/usuario', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/cadastrar-usuario', function (Request $request) {
-    // Dados passado via corpo da requisição
-    $data = $request->all();
-
-    //Validação de dados cadastrais
-    $validacao = Validator::make($data, [
-        'name' => 'required|string|max:255',
-        'email' => 'required|string|max:255|email|unique:users',
-        'password' => 'required|string|min:6|confirmed',
-    ]);
-    //Retorna os erros que possivelmente ocorrerão caso os requisitos da validação não forem atendidos
-    if ($validacao->fails()) {
-        return $validacao->errors();
-    };
-
-    $user = User::create([
-        'name' => $data['name'],
-        'email' => $data['email'],
-        'password' => Hash::make($data['password']),
-    ]);
-    $user->token = $user->createToken($user->email)->accessToken;
-    return $user;
-});
+Route::post('/cadastrar-usuario','CadastroUsuarioController@CadastrarUsuario');
 
 Route::get('/teste-metodo-api', function (Request $request) {
     return "Teste está funcionando";
