b40cba2810e69fbd331d7de9f6204d12b6d8885c
Joel <joelmaykon94@gmail.com>
2019-07-07 01:19:30 -0300

    Rota que é passado os dados no corpo da requisição e é retornado esses dados após a requisição

--- webservice-livros/routes/api.php
+++ webservice-livros/routes/api.php
@@ -11,10 +11,14 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-Route::get('/teste-metodo-api', function(Request $request){
+ */
+Route::get('/teste-metodo-api', function (Request $request) {
     return "Teste está funcionando";
-   });
+});
+// Retornando todos os dados passado via corpo da requisição
+Route::post('/post-teste-retorna-dados-form', function (Request $request) {
+    return $request->all();
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
