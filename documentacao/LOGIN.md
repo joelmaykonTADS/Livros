2bb6dac664cb4bcf3b898b29ee5d7e6fa63d3151
Joel <joelmaykon94@gmail.com>
2019-07-07 13:33:50 -0300

    Login do usuarioe retorno e token do usuário

---
+++ documentacao/LOGIN.md
@@ -0,0 +1,26 @@
+# Login na API
+
+Implementação do metodo HTTP/POST onde as informações são `email` e  `password` com algumas validações como requerido  o email e senha para concluir o login entre outras validações.
+```
+    //Validação de dados do login
+    $validacao = Validator::make($data, [
+        'email' => 'required|string|max:255|email',
+        'password' => 'required|string',
+    ]);
+    //Retorna os erros que possivelmente ocorrerão caso os requisitos da validação não forem atendidos
+    if ($validacao->fails()) {
+        return $validacao->errors();
+    };
+```
+Usando a classe Auth do Laravel para fazer a autenticação usando o método estático para fazer a validação.
+
+Se entrar no teste é poque o usuário teve suas credencias verificadas e autenticadas senão o usuário não estar autorizado:
+```
+    if(Auth::attemp(['email'=>$data['email'],'password'=>$data['password']])){
+        $user = auth()->user();
+        $user->token = $user->createToken($user->email)->accessToken;
+        return $user;
+    }else{
+        return ['status' => false, 'msg' => "Não autorizado"];
+    } 
+```
--- webservice-livros/routes/api.php
+++ webservice-livros/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\User;
+use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -17,31 +18,53 @@ use Illuminate\Support\Facades\Validator;
 | Todos os novos métodos são criados acima dos mais antigos.
  */
 
+Route::post('/login-usuario', function (Request $request) {
+    // Dados passado via corpo da requisição
+    $data = $request->all();
+
+    //Validação de dados do login
+    $validacao = Validator::make($data, [
+        'email' => 'required|string|max:255|email',
+        'password' => 'required|string',
+    ]);
+    //Retorna os erros que possivelmente ocorrerão caso os requisitos da validação não forem atendidos
+    if ($validacao->fails()) {
+        return $validacao->errors();
+    };
+    if(Auth::attempt(['email'=>$data['email'],'password'=>$data['password']])){
+        $user = auth()->user();
+        $user->token = $user->createToken($user->email)->accessToken;
+        return $user;
+    }else{
+        return ['status' => false, 'msg' => "Não autorizado"];
+    } 
+});
+
 Route::middleware('auth:api')->get('/usuario', function (Request $request) {
     return $request->user();
 });
 
 Route::post('/cadastrar-usuario', function (Request $request) {
-    // Dados passado pela reuisição via corpo
+    // Dados passado via corpo da requisição
     $data = $request->all();
 
     //Validação de dados cadastrais
-    $validacao = Validator::make($data,[
+    $validacao = Validator::make($data, [
         'name' => 'required|string|max:255',
         'email' => 'required|string|max:255|email|unique:users',
-        'password' =>'required|string|min:6|confirmed', 
+        'password' => 'required|string|min:6|confirmed',
     ]);
     //Retorna os erros que possivelmente ocorrerão caso os requisitos da validação não forem atendidos
-    if($validacao->fails()){
-        return  $validacao->errors();
+    if ($validacao->fails()) {
+        return $validacao->errors();
     };
 
     $user = User::create([
         'name' => $data['name'],
         'email' => $data['email'],
         'password' => Hash::make($data['password']),
-        ]);
-        $user->token = $user->createToken($user->email)->accessToken;
+    ]);
+    $user->token = $user->createToken($user->email)->accessToken;
     return $user;
 });
 
