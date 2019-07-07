237c205511eb4aa1d70121de816af910552183a4
Joel <joelmaykon94@gmail.com>
2019-07-07 14:39:33 -0300

    Controlador Login: método de login

---
+++ webservice-livros/app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Auth as auth;
+
+class UsuarioController extends Controller
+{
+    //Metodo login
+    public function login(Request $request)
+    {
+        // Dados passado via corpo da requisição
+        $data = $request->all();
+
+        //Validação de dados do login
+        $validacao = Validator::make($data, [
+            'email' => 'required|string|max:255|email',
+            'password' => 'required|string',
+        ]);
+        //Retorna os erros que possivelmente ocorrerão caso os requisitos da validação não forem atendidos
+        if ($validacao->fails()) {
+            return $validacao->errors();
+        };
+        if (Auth::attempt(['email' => $data['email'], 'password' => $data['password']])) {
+            $user = auth()->user();
+            $user->token = $user->createToken($user->email)->accessToken;
+            return $user;
+        } else {
+            return ['status' => false, 'msg' => "Não autorizado"];
+        }
+    }
+}
